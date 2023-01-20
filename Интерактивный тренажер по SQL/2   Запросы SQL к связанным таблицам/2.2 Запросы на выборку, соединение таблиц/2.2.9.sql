e8b42a414bf47cb9d49e10441c01f9615be30d0c
alni68 <alni68@mail.ru>
2023-01-20 15:15:02 +0400

    Create 2.2.9.sql
    Co-Authored-By: alni68 <72321089+alni68@users.noreply.github.com>

---
+++ Интерактивный тренажер по SQL/2   Запросы SQL к связанным таблицам/2.2 Запросы на выборку, соединение таблиц/2.2.9.sql
@@ -0,0 +1,14 @@
+Операция соединение, использование USING()
+
+Задание
+Если в таблицах supply  и book есть одинаковые книги, которые имеют равную цену,  вывести их название и автора, а также посчитать общее количество экземпляров книг в таблицах supply и book,  столбцы назвать Название, Автор  и Количество.
+
+Решение:
+SELECT book.title AS Название, name_author AS Автор, SUM(book.amount + supply.amount) AS Количество
+FROM 
+    author 
+    INNER JOIN book USING (author_id)   
+    INNER JOIN supply ON book.title = supply.title 
+                         AND author.name_author = supply.author
+    WHERE book.price = supply.price
+    GROUP BY book.title, name_author;
