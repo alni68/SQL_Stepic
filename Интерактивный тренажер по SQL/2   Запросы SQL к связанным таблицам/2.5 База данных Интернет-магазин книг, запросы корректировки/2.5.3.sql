549cd46637d328520d2012cac223677358b6f0ba
alni68 <alni68@mail.ru>
2023-01-22 14:19:05 +0400

    Create 2.5.3.sql

---
+++ Интерактивный тренажер по SQL/2   Запросы SQL к связанным таблицам/2.5 База данных Интернет-магазин книг, запросы корректировки/2.5.3.sql
@@ -0,0 +1,6 @@
+Задание
+Создать новый заказ для Попова Ильи. Его комментарий для заказа: «Связаться со мной по вопросу доставки».
+
+Решение:
+INSERT INTO buy (buy_description, client_id)
+SELECT 'Связаться со мной по вопросу доставки', (SELECT client_id FROM client WHERE name_client = 'Попов Илья');
