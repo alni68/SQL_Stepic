6362fb42f1f87544c24b0a34f4a6286cb8c59abd
alni68 <alni68@mail.ru>
2023-01-24 12:21:57 +0400

    Create 3.5.8.sql

---
+++ Интерактивный тренажер по SQL/3  Базы данных и SQL запросы/3.5 База данных Учебная аналитика по курсу/3.5.8.sql
@@ -0,0 +1,16 @@
+Оконные функции, оператор OVER, ORDER BY
+
+Задание
+Для студента с именем student_61 вывести все его попытки: название шага, результат и дату отправки попытки (submission_time). Информацию отсортировать по дате отправки попытки и указать, сколько минут прошло между отправкой соседних попыток. Название шага ограничить 20 символами и добавить "...". Столбцы назвать Студент, Шаг, Результат, Дата_отправки, Разница.
+
+Решение:
+SELECT student_name AS Студент, 
+    CONCAT(LEFT(step_name, 20), '...') AS Шаг, 
+    result AS Результат, 
+    FROM_UNIXTIME(submission_time) AS Дата_отправки,
+    SEC_TO_TIME(submission_time - LAG(submission_time, 1, submission_time) OVER (ORDER BY submission_time)) AS Разница
+FROM student
+    JOIN step_student USING(student_id)
+    JOIN step USING(step_id)
+WHERE student_name = 'student_61'
+ORDER BY Дата_отправки;
